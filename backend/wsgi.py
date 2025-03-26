baa074e8e8edededa492195aabd1fb3ce045c709
Devashish <182228214+PatelDevashish001@users.noreply.github.com>
2025-03-26 22:37:59 +0530

    Update wsgi.py

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -1,4 +1,6 @@
 from backend.app import app
+import os  # Import the 'os' module
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=False)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment, default to 5000
+    app.run(host="0.0.0.0", port=port, debug=False)
